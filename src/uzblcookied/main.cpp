355a443c259d151ba4ef6fce774981b64e438852
Paweł Zuzelski <pawelz@pld-linux.org>
2010-05-31 13:43:07 +0200

    uzblcookied: better use of lockfile
    Create lockfile in XDG_CACHE_DIR. It will allow to use coockied simultanously
    by multiple users. It will also make possible to have two separate sessions of
    uzbl with separate cookies managemnet. Just specify different XDG_*_HOME envs.
    
    Write pid to lockfile. It does not cost a lot and may be useful for scripts.

--- src/uzblcookied/main.cpp
+++ src/uzblcookied/main.cpp
@@ -1,6 +1,9 @@
 #include "cookiejar.h"
 #include <signal.h>
 #include <sys/stat.h>
+#include <basedir.h>
+#include <unistd.h>
+#include <cstring>
 
 void sigtermhandle(int a)
 {
@@ -10,13 +13,25 @@ void sigtermhandle(int a)
 int main()
 {
     
-    int fd = open("/tmp/uzblcookied", O_RDWR|O_CREAT);
-    fchmod(fd, 0666);
+    /* Create pidfile and use it as lock */
+    xdgHandle xdg;
+    xdgInitHandle(&xdg);
+    char pidfile[1024*4];
+    memset(pidfile, 0, 1024*4);
+    sprintf(pidfile, "%s/uzbl/uzblcookied", xdgCacheHome(&xdg));
+    xdgWipeHandle(&xdg);
+    int fd = open(pidfile, O_RDWR|O_CREAT, 0600);
     
     flock* l = new flock;
     l->l_type = F_WRLCK;
     l->l_whence = SEEK_SET;
     
+    char pid[16];
+    memset(pidfile, 0, 16);
+    sprintf(pid, "%i\n", getpid());
+    write(fd, pid, strlen(pid));
+    fsync(fd);
+
     if (fcntl(fd, F_SETLK, l) == -1)
         return 1;
     
@@ -32,6 +47,8 @@ int main()
         cookiejar->WriteFile();
     }
     delete cookiejar;
+    close(fd);
+    unlink(pidfile);
     
     return 1;
 }
